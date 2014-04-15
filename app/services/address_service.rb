73ba55eb203a2d7ca34dce6c7d69d036335a46fb
Danny Hawkins <danny.hawkins@gmail.com>
2014-04-15 15:03:19 +0100

    added logging

--- app/services/address_service.rb
+++ app/services/address_service.rb
@@ -8,11 +8,15 @@ class AddressService
   #   @options = { :query => {:site => service, :page => page} }
   # end
   def resolve_postcode(postcode)
-    self.class.get("/partial/#{CGI::escape(postcode.gsub(' ',''))}")
+    Rails.logger.info("Searching for #{postcode}")
+    response = self.class.get("/partial/#{CGI::escape(postcode.gsub(' ',''))}")
+    Rails.logger.info("Found #{response}")
+    response
   end
 
   def stations_for_postcode(postcode)
     result = resolve_postcode(postcode)
+    
     Station.where( :coordActual.near => [result['easting'], result['northing']])
   end
 
