651e97f04d8c84bf5a4d3f4d9b222bd15793193d
Danny Hawkins <danny.hawkins@gmail.com>
2014-04-15 13:30:06 +0100

    updated production

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'floodapi'
+set :repo_url, 'git@github.com:danhawkins/floodapi.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, "/srv/www/floodapi"
 
 # Default value for :scm is :git
 # set :scm, :git
@@ -32,7 +32,7 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 namespace :deploy do
 
