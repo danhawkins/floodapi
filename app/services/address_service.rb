ec9c197b98b710b99ef499db7281805575eceb58
Danny Hawkins <danny.hawkins@gmail.com>
2014-04-15 15:07:14 +0100

    updated rbenve

--- app/services/address_service.rb
+++ app/services/address_service.rb
@@ -8,15 +8,16 @@ class AddressService
   #   @options = { :query => {:site => service, :page => page} }
   # end
   def resolve_postcode(postcode)
+    postcode.gsub!(/\s/,'')
     Rails.logger.info("Searching for #{postcode}")
-    response = self.class.get("/partial/#{CGI::escape(postcode.gsub(' ',''))}")
+    response = self.class.get("/partial/#{postcode}")
     Rails.logger.info("Found #{response}")
     response
   end
 
   def stations_for_postcode(postcode)
     result = resolve_postcode(postcode)
-    
+
     Station.where( :coordActual.near => [result['easting'], result['northing']])
   end
 
